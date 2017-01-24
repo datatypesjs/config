c5cf8b4a91004b2d3e22296583ad1a32fa3cec79
Adrian Sieber <mail@adriansieber.com>
2017-01-24 21:30:03 +0000

    Add support for custom env prefix & separators

--- index.js
+++ index.js
@@ -5,25 +5,48 @@ module.exports = class Config {
   constructor (options = {}) {
     Object.assign(this, options)
     this._config = {}
+
+    this.env = {
+      prefix: '',
+      pathSeparator: '__',
+      wordSeparator: '_',
+      casing: 'camel',
+    }
   }
 
-  loadEnvironment () {
+  loadEnvironment (options = {}) {
+    Object.assign(this.env, options)
+
     const environment = Object.assign({}, process.env)
     Object
       .keys(environment)
       .forEach(envVar => {
-        if (!envVar.startsWith(this.envPrefix)) return
-        const envVarNoPrefix = envVar.replace(this.envPrefix, '')
-        const fields = envVarNoPrefix.split('__')
+        if (!envVar.startsWith(this.env.prefix)) return
+
+        const envVarNoPrefix = envVar.replace(this.env.prefix, '')
+        const fields = envVarNoPrefix.split(this.env.pathSeparator)
         const object = {}
         let temp = object
+
         fields.forEach((field, index) => {
           field = field.toLowerCase()
+
+          if (this.env.casing === 'camel') {
+            field = field.replace(
+              /\_(.)/g,
+              (match, firstChar) => firstChar.toUpperCase()
+            )
+          }
+          else {
+            throw new Error(`"${this.env.casing}" is not supported`)
+          }
+
           temp[field] = index === fields.length - 1
             ? environment[envVar]
             : {}
           temp = temp[field]
         })
+
         this._config = merge(this._config, object)
       })
   }
--- tests/main.js
+++ tests/main.js
@@ -1,17 +1,33 @@
 const expect = require('unexpected')
-const expectedConfig = {
-  value: 'something',
+const Config = require('..')
+
+
+{
+  process.env.CONFIGTEST_VALUE = 'something'
+
+  const expectedConfig = {value: 'something'}
+  const config = new Config()
+  config.loadEnvironment({prefix: 'CONFIGTEST_'})
+
+  expect(config.object, 'to equal', expectedConfig)
+
+  delete process.env.CONFIGTEST_VALUE
 }
 
-process.env.CONFIGTEST_VALUE = 'something'
+{
+  process.env.CONFIGTEST_OBJECT__WITH__SUB_FIELDS = 'another thing'
 
+  const expectedConfig = {
+    object: {
+      with: {
+        subFields: 'another thing',
+      },
+    },
+  }
+  const config = new Config()
+  config.loadEnvironment({prefix: 'CONFIGTEST_'})
 
-const Config = require('..')
-const config = new Config({
-  envPrefix: 'CONFIGTEST_',
-})
+  expect(config.object, 'to equal', expectedConfig)
 
-config
-  .loadEnvironment()
-
-expect(config.object, 'to equal', expectedConfig)
+  delete process.env.CONFIGTEST_OBJECT__WITH__SUB_FIELDS
+}
